f8111f8a0d7cda356a4597ecf63398d4867885ee
DETorz <137002616+DETorz@users.noreply.github.com>
2025-01-29 19:51:50 +0000

    Assignment complete

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,25 @@
-
+import java.util.Scanner;
+public class Input
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int age;
+        float weight;
+        boolean smoker;
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+        System.out.print("Your age: ");
+        age = scanner.nextInt();
+        System.out.print("Your weight: ");
+        weight = scanner.nextFloat();
+        System.out.print("(true/false) Do you smoke tabacco? ");
+        smoker = scanner.nextBoolean();
+        System.out.println("Your answers have been recorded: ");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Humanly mass (in lbs): " + weight);
+        System.out.println("Smoker? " + smoker);
+    }
+}
